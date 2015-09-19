8c30110e9fbf13b6ccc81cddb781dbed1865a45f
Pedro Putz <pedroputz@gmail.com>
2015-09-18 22:34:14 -0300

    Adding touch commands to rebuild icon cache after change it

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local
 
 echo "Changing icon"
 cp -f Sublime\ Text.icns /Applications/Sublime\ Text.app/Contents/Resources/Sublime\ Text.icns
-
+touch /Applications/Sublime\ Text.app
+touch /Applications/Sublime\ Text.app/Contents/Info.plist
